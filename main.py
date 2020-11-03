db8fc6fa452fad46066bec3bd48462fa447ecd95
zfima <zzfima@gmail.com>
2020-11-03 11:02:09 +0200

    init

--- main.py
+++ main.py
@@ -1,6 +1,15 @@
 from selenium import webdriver
+from selenium.webdriver.common.by import By
 
-site = input()
+site = input('input site')
+min_friends_amount = input('input minimal other shared connections')
 driver = webdriver.Chrome()
 driver.get(site)
+
+while True:
+    el = driver.find_elements_by_class_name("li")
+
+    for a in el:
+        pass
+
 driver.close()
